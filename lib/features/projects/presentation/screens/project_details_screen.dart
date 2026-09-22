924044624d78937eadb026776e5635ded0b26eb2
k8n3j <vertexdigitalweb100@gmail.com>
2026-09-22 20:45:24 +0500

    fix: rebuild client section when client state changes

--- lib/features/projects/presentation/screens/project_details_screen.dart
+++ lib/features/projects/presentation/screens/project_details_screen.dart
@@ -541,8 +541,21 @@ class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
   }
 
   Widget _buildClientSection(BuildContext context) {
-    final clientProvider = _clientProvider;
-    final client = clientProvider.clientForProject(project.id);
+    return ListenableBuilder(
+      listenable: _clientProvider,
+      builder: (context, _) {
+        final clientProvider = _clientProvider;
+        final client = clientProvider.clientForProject(project.id);
+        return _buildClientCard(context, clientProvider, client);
+      },
+    );
+  }
+
+  Widget _buildClientCard(
+    BuildContext context,
+    ClientProvider clientProvider,
+    Client? client,
+  ) {
     final colorScheme = Theme.of(context).colorScheme;
 
     final clientTextColor = colorScheme.onSurface;
